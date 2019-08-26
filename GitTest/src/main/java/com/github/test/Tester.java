0cd590fb11ccfee9b3d5ce78b6dae4a24db6014b
soldesk <soldesk@soldesk-PC>
2019-08-26 15:28:12 +0900

    tester1-3
    tester1-3

--- GitTest/src/main/java/com/github/test/Tester.java
+++ GitTest/src/main/java/com/github/test/Tester.java
@@ -3,7 +3,7 @@ package com.github.test;
 public class Tester {
 	public static void main(String[] args) {
 		System.out.println("실험시작.");
-		System.out.println("tester1");
+		System.out.println("tester1-2");
 		System.out.println("tester2-2");
 	}
 }
